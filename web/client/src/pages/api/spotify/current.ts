eaa42dd4b1986b806ea1008a667927cd14a004f8
Justin Johnson <justinaugustejohnson@gmail.com>
2025-05-06 03:21:43 -0400

    bug fix

--- web/client/src/pages/api/spotify/current.ts
+++ web/client/src/pages/api/spotify/current.ts
@@ -1,10 +1,9 @@
-
 import type { APIRoute } from 'astro';
 import { createClient } from '@supabase/supabase-js';
 import SpotifyWebApi from 'spotify-web-api-node';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL or Anon Key not configured.");
